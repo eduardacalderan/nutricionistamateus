2f68bd43958e4bb8d89eda2416eabe357aed78ec
Eduarda Calderan <85559533+eduardacalderan@users.noreply.github.com>
2021-08-03 11:17:22 -0300

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,65 @@
+/*abre, fecha o menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*esconde o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    //maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/* carousel/slider/swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar'
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  keyboard: true
+})
+
+/* back to top */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 860) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* when scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
